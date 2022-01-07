49d058a188658eacdaa023cc3cfd36208e7f15a3
Miha <mp6079@student.uni-lj.si>
2022-01-07 17:21:30 +0100

    added discovery service

--- api/src/main/java/si/fri/rso/uniborrow/blogs/api/v1/BlogsApplication.java
+++ api/src/main/java/si/fri/rso/uniborrow/blogs/api/v1/BlogsApplication.java
@@ -3,6 +3,9 @@ package si.fri.rso.uniborrow.blogs.api.v1;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
+@RegisterService(value = "uniborrow-blogs-service", environment = "dev", version = "1.0.0")
 @ApplicationPath("/v1")
 public class BlogsApplication extends Application {
 
